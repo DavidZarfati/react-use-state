abe200d9ce0fa7da54a31852d4e90a9c508e7e51
DavidZarfati <zarda2001@gmail.com>
2025-11-28 17:11:30 +0100

    feat:commit dopo le magnifiche correzioni dello zio Loris

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import './App.css'
 import languages from "../languages"
 
 function App() {
-  const [openId, setOpenId] = useState(1);
+
+  const [openId, setOpenId] = useState();
   return (
     <>
       <header className='titolo'><h1>Learn web development</h1></header>
       <div>
         {languages.map((language) => (
-          <button className='btn' key={language.id}>{language.title}</button>
-        ))
-        }
+          <button
+            className='btn'
+            key={language.id}
+            onClick={() => setOpenId(language)}
+          >
+            {language.title}
+          </button>
+        ))}
 
         <div className="card">
-          <h3>nome linguaggio</h3>
-          <p>spiegazione linguaggio</p>
+          <h3>{openId ? openId.title : "Nessun linguaggio Selezionato"}</h3>
+          <p>{openId ? openId.description : "Nessun linguaggio Selezionato"}</p>
         </div>
 
       </div>
